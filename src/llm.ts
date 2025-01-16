cf084eff670c3e5e157d50450bfc349b63b74ac8
Adnan <adbak.dev@gmail.com>
2025-01-16 15:16:35 +0100

    test

---
+++ src/llm.ts
@@ -0,0 +1,11 @@
+import { string } from 'zod'
+import { openai } from './ai'
+
+export const runLLM = async ({ userMessage }: { userMessage: string }) => {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    messages: [{ role: 'user', content: userMessage }],
+  })
+
+  return response.choices[0].message.content
+}
